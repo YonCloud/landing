c0e5ffaa918d003ea17a4b0fbcdf6a55b2494dd8
YonCloud <jhon.vargasr@gmail.com>
2020-03-03 16:33:55 -0500

    first commit

---
+++ precache-manifest.c9df6d9674a117b5394e3cca0ba9d326.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716e49f265e23a2efe1ce95e69293528",
+    "url": "/yamheru/index.html"
+  },
+  {
+    "revision": "1d4417d112a4abac50b1",
+    "url": "/yamheru/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "03d3806c39867f202966",
+    "url": "/yamheru/static/css/main.8df402a5.chunk.css"
+  },
+  {
+    "revision": "1d4417d112a4abac50b1",
+    "url": "/yamheru/static/js/2.10ecd8e4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/yamheru/static/js/2.10ecd8e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d3806c39867f202966",
+    "url": "/yamheru/static/js/main.3d807029.chunk.js"
+  },
+  {
+    "revision": "49a5c5d610365c3dae3a",
+    "url": "/yamheru/static/js/runtime-main.352a942f.js"
+  },
+  {
+    "revision": "41a4272ee454d951d4ce0833c41c3c94",
+    "url": "/yamheru/static/media/complementos.41a4272e.jpg"
+  },
+  {
+    "revision": "33dc0b0d3a6ddd2bcc3136321d0bd4dc",
+    "url": "/yamheru/static/media/fondo.33dc0b0d.jpg"
+  },
+  {
+    "revision": "dfd862880b63b5076247b2531e548f2e",
+    "url": "/yamheru/static/media/fondo1.dfd86288.jpg"
+  },
+  {
+    "revision": "c815ee0b69eaa225dc0a3093c7bfc611",
+    "url": "/yamheru/static/media/mesa2.c815ee0b.png"
+  },
+  {
+    "revision": "bfa8252cbc5b2e9bd84c9da12cdfec65",
+    "url": "/yamheru/static/media/planta1.bfa8252c.jpg"
+  },
+  {
+    "revision": "95e02083a6a4913126eb4c295f2e6a49",
+    "url": "/yamheru/static/media/ramo21.95e02083.jpg"
+  },
+  {
+    "revision": "33abd1271d360e4aabe98b1b17a7052f",
+    "url": "/yamheru/static/media/text777.33abd127.jpg"
+  },
+  {
+    "revision": "73e6fe3d2a59b4f8dbd2bf5122045431",
+    "url": "/yamheru/static/media/text869.73e6fe3d.png"
+  }
+]);
